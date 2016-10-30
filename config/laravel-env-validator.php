517bfb4d98dbfd1f4e3bd0d4ef4fdcd2dffeae87
lopadova <lorenzo.padovani@padosoft.com>
2016-10-30 14:19:52 +0100

    initial commit

---
+++ config/laravel-env-validator.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: Mathias Grimm <mathiasgrimm@gmail.com>
+ * Date: 07/02/2016
+ * Time: 18:34
+ */
+
+return [
+    // 'SOME_IMPORTANT_VARIABLE' => 'required',
+    // 'ANOTHER_IMPORTANT_ONE'   => 'required|in:TYPE_A,TYPE_B,TYPE_C',
+];
